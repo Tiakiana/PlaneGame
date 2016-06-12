3e4b093564d2b681e6fc68a5fc35c7f559fe4c61
jakob arndal <takana@exelion.dk>
2016-06-12 20:47:09 +0200

    RoboRally To the mix
    Jakob

---
+++ RoboRally/Assets/Movement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void DriveForward()
+    {
+        transform.position += transform.right;
+    }
+    void DriveBackward()
+    {
+        transform.position -= transform.right;
+    }
+
+    void TurnLeft()
+    {
+
+        transform.RotateAround(transform.position,Vector3.forward,90);
+    }
+
+    void TurnRight()
+    {
+        transform.RotateAround(transform.position, Vector3.forward, -90);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if (Input.GetKeyUp("w"))
+	    {
+	        DriveForward();
+	    }
+        if (Input.GetKeyUp("s"))
+        {
+            DriveBackward();
+        }
+        if (Input.GetKeyUp("a"))
+        {
+            TurnLeft();
+        }
+        if (Input.GetKeyUp("d"))
+        {
+            TurnRight();
+        }
+
+    }
+}
